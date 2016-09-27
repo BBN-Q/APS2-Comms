fef52dffe26ddbc578fa7043f4d163755fd42b4a
Colm Ryan <colm@colmryan.org>
2016-09-26 20:29:44 -0400

    explicitly call python - mainly for CI Docker compatibility

--- scripts/add_files_to_project.tcl
+++ scripts/add_files_to_project.tcl
@@ -11,9 +11,9 @@ update_ip_catalog -rebuild
 set cur_dir [pwd]
 cd $APS2_COMMS_REPO_PATH/deps
 file mkdir build
-exec python verilog-axis/rtl/axis_mux.py --ports=3 --output=build/axis_mux_3.v
-exec python verilog-axis/rtl/axis_arb_mux.py --ports=3 --output=build/axis_arb_mux_3.v
-exec python verilog-axis/rtl/axis_demux.py --ports=2 --output=build/axis_demux_2.v
+exec python3 verilog-axis/rtl/axis_mux.py --ports=3 --output=build/axis_mux_3.v
+exec python3 verilog-axis/rtl/axis_arb_mux.py --ports=3 --output=build/axis_arb_mux_3.v
+exec python3 verilog-axis/rtl/axis_demux.py --ports=2 --output=build/axis_demux_2.v
 
 # patch demux because select is keyword in VHDL
 set fp [open build/axis_demux_2.v r]
