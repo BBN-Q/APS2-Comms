7c18fcea7a5ae05b8982cc600d5bba93ffaf8b5f
Colm Ryan <colm@colmryan.org>
2016-09-28 08:41:22 -0400

    just import verilog_axis build products into projects
    avoids dirty submodule issues

--- scripts/add_files_to_project.tcl
+++ scripts/add_files_to_project.tcl
@@ -9,20 +9,27 @@ update_ip_catalog -rebuild
 
 # create dependency outputs
 set cur_dir [pwd]
-cd $APS2_COMMS_REPO_PATH/deps
-file mkdir build
-exec python3 verilog-axis/rtl/axis_mux.py --ports=3 --output=build/axis_mux_3.v
-exec python3 verilog-axis/rtl/axis_arb_mux.py --ports=3 --output=build/axis_arb_mux_3.v
-exec python3 verilog-axis/rtl/axis_demux.py --ports=2 --output=build/axis_demux_2.v
+cd $APS2_COMMS_REPO_PATH/deps/verilog-axis/rtl
+exec python3 axis_mux.py --ports=3 --output=axis_mux_3.v
+exec python3 axis_arb_mux.py --ports=3 --output=axis_arb_mux_3.v
+exec python3 axis_demux.py --ports=2 --output=axis_demux_2.v
 
 # patch demux because select is keyword in VHDL
-set fp [open build/axis_demux_2.v r]
+set fp [open axis_demux_2.v r]
 set demux [read $fp]
 close $fp
 regsub -all {select} $demux control demux
-set fp [open build/axis_demux_2.v w]
+set fp [open axis_demux_2.v w]
 puts -nonewline $fp $demux
 close $fp
+
+#import into project and then delete to avoid dirty submodule
+import_files -norecurse -flat \
+	$APS2_COMMS_REPO_PATH/deps/verilog-axis/rtl/axis_demux_2.v \
+	$APS2_COMMS_REPO_PATH/deps/verilog-axis/rtl/axis_mux_3.v \
+	$APS2_COMMS_REPO_PATH/deps/verilog-axis/rtl/axis_arb_mux_3.v
+
+file delete axis_demux_2.v axis_mux_3.v axis_arb_mux_3.v
 
 # patch the Com5402 module for UDP broadcast issue and add DHCP module
 cd $APS2_COMMS_REPO_PATH/deps/ComBlock/5402
@@ -54,11 +61,6 @@ add_files -norecurse \
 	$APS2_COMMS_REPO_PATH/deps/verilog-axis/rtl/priority_encoder.v
 
 add_files -norecurse \
-	$APS2_COMMS_REPO_PATH/deps/build/axis_demux_2.v \
-	$APS2_COMMS_REPO_PATH/deps/build/axis_mux_3.v \
-	$APS2_COMMS_REPO_PATH/deps/build/axis_arb_mux_3.v
-
-add_files -norecurse \
 	$APS2_COMMS_REPO_PATH/deps/verilog-ethernet/rtl/eth_mac_1g_fifo.v \
 	$APS2_COMMS_REPO_PATH/deps/verilog-ethernet/rtl/eth_mac_1g.v \
 	$APS2_COMMS_REPO_PATH/deps/verilog-ethernet/rtl/eth_mac_1g_rx.v \
@@ -85,10 +87,13 @@ add_files -norecurse \
 	$APS2_COMMS_REPO_PATH/deps/ComBlock/5402/udp_tx.vhd \
 	$APS2_COMMS_REPO_PATH/deps/ComBlock/5402/whois2.vhd
 
+
+update_compile_order -fileset sources_1
+update_compile_order -fileset sim_1
+
 # constraints (have to add tcl files separately)
 add_files -fileset constrs_1 -norecurse $APS2_COMMS_REPO_PATH/constraints
 add_files -fileset constrs_1 -norecurse $APS2_COMMS_REPO_PATH/constraints/async_fifos.tcl
-
 
 source $APS2_COMMS_REPO_PATH/src/bd/aps2_comms_bd.tcl
 regenerate_bd_layout
